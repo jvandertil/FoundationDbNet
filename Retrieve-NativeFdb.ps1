dc84d5ee56146adc3cecf2ef042086d4eb05a0df
Jos van der Til <jos@vandertil.net>
2018-06-22 12:14:43 +0200

    Add alternative for BITS to Retrieve-NativeFdb.ps1
    The Background Intelligent Transfer Service (BITS) is not always
    available. On build servers this service might not be started, or
    because of the way agents execute it might not be available. When the
    BITS service is not running, it will use the .NET WebClient to download
    the native libraries.
    
    Also added a check to avoid creating the directory multiple times if it
    already exists.

--- Retrieve-NativeFdb.ps1
+++ Retrieve-NativeFdb.ps1
@@ -1,2 +1,37 @@
-mkdir $PSScriptRoot\native
-Start-BitsTransfer -Source "https://www.foundationdb.org/downloads/5.1.7/windows/libfdb_c_5.1.7.dll" -Destination $PSScriptRoot\native\libfdb_c_5.1.7.dll
+function Download-File($Url, $Destination) 
+{
+    $bitsService = Get-Service BITS
+    
+    if ( ! ($bitsService.Status -like 'Running'  ) )
+    {
+        Write-Host "BITS appears to be disabled, using WebClient"
+    
+        try
+        {
+            $wc = New-Object System.Net.WebClient
+            $wc.DownloadFile($Url, $Destination)
+        } finally {
+            $wc.Dispose()
+        }
+    }
+    else
+    {
+        Start-BitsTransfer -Source $Url -Destination $Destination
+    }
+}
+
+$nativeFolder = "$PSScriptRoot\native"
+
+$fdbVersion = "5.1.7"
+$dllName = "libfdb_c_$fdbVersion.dll"
+
+$url = "https://www.foundationdb.org/downloads/$fdbVersion/windows/$dllName"
+$destination = "$PSScriptRoot\native\$dllName"
+
+if( ! (Test-Path $nativeFolder) )
+{
+    mkdir $nativeFolder
+}
+
+Download-File -Url $url -Destination $destination
+
