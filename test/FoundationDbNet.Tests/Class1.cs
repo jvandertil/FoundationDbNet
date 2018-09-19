3b7e09c5dc87fd6adc6133a1ff64b7c495f6dd02
Jos van der Til <jos@vandertil.net>
2018-09-19 19:36:06 +0200

    Add minimal executing project

---
+++ test/FoundationDbNet.Tests/Class1.cs
@@ -0,0 +1,13 @@
+﻿namespace FoundationDbNet.Tests
+{
+    using Xunit;
+
+    public class Class1
+    {
+        [Fact]
+        public void Test()
+        {
+            Assert.True(true);
+        }
+    }
+}
