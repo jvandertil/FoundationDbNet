a7973aa9bc04d6a8814ba7918728185e4d4ba4cc
Jos van der Til <jos@vandertil.net>
2018-11-16 23:22:18 +0100

    Add Tuple encoder for integers

---
+++ src/FoundationDbNet/Layers/Tuple/IntegerEncoder.cs
@@ -0,0 +1,93 @@
+﻿namespace FoundationDbNet.Layers.Tuple
+{
+    using System;
+    using System.Runtime.InteropServices;
+
+    public class IntegerEncoder : IEncoder<long>
+    {
+        private const byte ZeroValueByte = 0x14;
+
+        private static readonly byte[] ZeroValue = { ZeroValueByte };
+        private static readonly ulong[] SizeLimits;
+
+        static IntegerEncoder()
+        {
+            unchecked
+            {
+                SizeLimits = new[]
+                {
+                    (1UL << 08) - 1,
+                    (1UL << 16) - 1,
+                    (1UL << 24) - 1,
+                    (1UL << 32) - 1,
+                    (1UL << 40) - 1,
+                    (1UL << 48) - 1,
+                    (1UL << 56) - 1,
+                    (1UL << 64) - 2,
+                };
+            }
+        }
+
+        public ReadOnlySpan<byte> Encode(long value)
+        {
+            if (value == 0)
+            {
+                return ZeroValue;
+            }
+
+            if (value > 0)
+            {
+                return EncodePositive(value);
+            }
+
+            return EncodeNegative(value);
+        }
+
+        private ReadOnlySpan<byte> EncodePositive(long value)
+        {
+            int size = GetEncodedSizeInBytes((ulong)value);
+
+            Span<byte> result = new byte[8 + 1];
+            result[0] = (byte)(ZeroValueByte + size);
+
+            var destination = result.Slice(1);
+
+            MemoryMarshal.Write(destination, ref value);
+            destination.Slice(0, size).Reverse();
+
+            return result.Slice(0, size + 1);
+        }
+
+        private ReadOnlySpan<byte> EncodeNegative(long value)
+        {
+            int size = GetEncodedSizeInBytes((ulong)-value);
+
+            Span<byte> result = new byte[8 + 1];
+            result[0] = (byte)(ZeroValueByte - size);
+
+            var destination = result.Slice(1);
+
+            var onesComplementValue = ((long)SizeLimits[size - 1]) + value;
+
+            MemoryMarshal.Write(destination, ref onesComplementValue);
+            destination.Slice(0, size).Reverse();
+
+            return result.Slice(0, size + 1);
+        }
+
+        private int GetEncodedSizeInBytes(ulong value)
+        {
+            int size = SizeLimits.Length;
+            for (int i = 0; i < SizeLimits.Length; ++i)
+            {
+                if (value <= SizeLimits[i])
+                {
+                    size = i + 1;
+                    break;
+                }
+            }
+
+            return size;
+        }
+    }
+}
---
+++ test/FoundationDbNet.Tests/Features/Layers/Tuple/IntegerEncoderTests.cs
@@ -0,0 +1,45 @@
+﻿namespace FoundationDbNet.Tests.Features.Layers.Tuple
+{
+    using FoundationDbNet.Layers.Tuple;
+    using FoundationDbNet.Tests.Framework.Layers.Tuple;
+    using Xunit;
+
+    public class IntegerEncoderTests : EncoderTestsBase<long>
+    {
+        public IntegerEncoderTests()
+            : base(new IntegerEncoder())
+        {
+        }
+
+        [Theory]
+        [InlineData(0, "0x14")] // Technically not positive, but whatever ;).
+        [InlineData(128, "0x1580")]
+        [InlineData(0xFF, "0x15FF")]
+        [InlineData(16801, "0x1641A1")]
+        [InlineData(0xFFFF, "0x16FFFF")]
+        [InlineData(14727200, "0x17E0B820")]
+        [InlineData(0xFFFFFF, "0x17FFFFFF")]
+        [InlineData(1455720000, "0x1856C48640")]
+        [InlineData(0xFFFFFFFF, "0x18FFFFFFFF")]
+        [InlineData(0x7FFFFFFFFFFFFFFF, "0x1C7FFFFFFFFFFFFFFF")]
+
+        public void CanEncodePositiveLongValues(long value, string expected)
+            => EncodeAndVerify(value, expected);
+
+        [Theory]
+        [InlineData(-1, "0x13FE")]
+        [InlineData(-128, "0x137F")]
+        [InlineData(-0xFF, "0x1300")]
+        [InlineData(-16801, "0x12BE5E")]
+        [InlineData(-0xFFFF, "0x120000")]
+        [InlineData(-14727200, "0x111F47DF")]
+        [InlineData(-0xFFFFFF, "0x11000000")]
+        [InlineData(-1455720000, "0X10A93B79BF")]
+        [InlineData(-0xFFFFFFFF, "0x1000000000")]
+        [InlineData(-6273172034854775807, "0X0CA8F137455F911400")]
+        [InlineData(-9223372036854775808, "0X0C7FFFFFFFFFFFFFFF")]
+        public void CanEncodeNegativeLongValues(long value, string expected)
+            => EncodeAndVerify(value, expected);
+    }
+
+}
